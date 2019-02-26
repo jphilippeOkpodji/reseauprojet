f7068dceaa583c915ec30e85fd26df9b7afe9794
jphilippeOkpodji <ledjipus789456123>
2019-02-26 02:04:20 +0100

    création fonction envoi message dans le fichier client et findMessage et findPseudo dans le fichier serveur

--- client.c
+++ client.c
@@ -33,3 +33,17 @@ void appli(){
   
 }
 
+int envoi_message(int sock_serv, char data[], int tailleData) {
+
+    if (strstr(data, "/name")) {
+        data[tailleData - 1] = '\0';
+        tailleData--;
+    } else if (strstr(data, "/quit")) {
+        write(sock_serv, data, tailleData);
+        close(sock_serv);
+        exit(EXIT_SUCCESS);
+    }
+
+    write(sock_serv, data, tailleData);
+    return EXIT_SUCCESS;
+}
